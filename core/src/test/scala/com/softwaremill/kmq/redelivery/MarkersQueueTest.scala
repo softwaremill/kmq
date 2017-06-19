7c2ddec39512bb6a180f8c1ac3c676a5b80b9fbc
adamw <adam@warski.org>
2017-06-19 16:29:18 +0200

    Additional test

--- core/src/test/scala/com/softwaremill/kmq/redelivery/MarkersQueueTest.scala
+++ core/src/test/scala/com/softwaremill/kmq/redelivery/MarkersQueueTest.scala
@@ -18,4 +18,18 @@ class MarkersQueueTest extends FlatSpec with Matchers {
     toRedeliver should have size (1)
     toRedeliver.head.getMessageOffset should be (1000)
   }
+
+  it should "redeliver multiple markers" in {
+    // given
+    val mq = new MarkersQueue(0)
+    mq.handleMarker(100, new MarkerKey(1, 1000), new StartMarker(4000L), 10000L)
+    mq.handleMarker(101, new MarkerKey(1, 1001), new StartMarker(4000L), 12000L)
+
+    // when
+    val toRedeliver = mq.markersToRedeliver(17000)
+
+    // then
+    toRedeliver should have size (2)
+    toRedeliver.map(_.getMessageOffset).toSet should be (Set(1001L, 1000L))
+  }
 }
