689e5d74792521477a63f81accffc610828cd7fd
MohamedEL59 <37627130+MohamedEL59@users.noreply.github.com>
2018-04-13 11:43:55 +0200

    Update RedeliveryActors.scala

--- core/src/main/scala/com.softwaremill.kmq/redelivery/RedeliveryActors.scala
+++ core/src/main/scala/com.softwaremill.kmq/redelivery/RedeliveryActors.scala
@@ -9,11 +9,17 @@ import com.typesafe.scalalogging.StrictLogging
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
+import scala.collection.JavaConverters._
+
 object RedeliveryActors extends StrictLogging {
   def start(clients: KafkaClients, config: KmqConfig): Closeable = {
+    start(clients, config)
+  }
+
+  def start(clients: KafkaClients, config: KmqConfig, extraConfig: Option[java.util.Map[String, Object]] = None): Closeable = {
     val system = ActorSystem("kmq-redelivery")
 
-    val consumeMakersActor = system.actorOf(Props(new ConsumeMarkersActor(clients, config)), "consume-markers-actor")
+    val consumeMakersActor = system.actorOf(Props(new ConsumeMarkersActor(clients, config, extraConfig)), "consume-markers-actor")
     consumeMakersActor ! DoConsume
 
     logger.info("Started redelivery actors")
