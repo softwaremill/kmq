742c37fd7a17d3f6f89f23a373569af50bc2d281
MohamedEL59 <37627130+MohamedEL59@users.noreply.github.com>
2018-04-13 11:36:07 +0200

    Update KmqClient.java

--- core/src/main/java/com/softwaremill/kmq/KmqClient.java
+++ core/src/main/java/com/softwaremill/kmq/KmqClient.java
@@ -10,7 +10,6 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.Option;
 
 import java.io.Closeable;
 import java.io.IOException;
