d5eec05a2f988db6871f17b16350b8f64e3f3014
Mariusz Krzemień <mkrzemien@gmail.com>
2022-07-12 19:46:14 +0200

    start marker stream - committablePartitionedSource

--- core/src/main/scala/com/softwaremill/kmq/redelivery/streams/StartMarkerStream.scala
+++ core/src/main/scala/com/softwaremill/kmq/redelivery/streams/StartMarkerStream.scala
@@ -6,6 +6,7 @@ import akka.kafka.ConsumerMessage.CommittableMessage
 import akka.kafka.scaladsl.Consumer.DrainingControl
 import akka.kafka.scaladsl.{Committer, Consumer, Producer}
 import akka.kafka._
+import akka.stream.scaladsl.Sink
 import com.softwaremill.kmq.{MarkerKey, MarkerValue, StartMarker}
 import org.apache.kafka.clients.producer.ProducerRecord
 
@@ -17,20 +18,22 @@ class StartMarkerStream(consumerSettings: ConsumerSettings[String, String],
   def run(): DrainingControl[Done] = {
     val committerSettings = CommitterSettings(system)
 
-    Consumer.committableSource(consumerSettings, Subscriptions.topics(queueTopic))
-      // TODO: use partitionedSource instead of groupBy
-      .groupBy(maxSubstreams = maxPartitions, f = msg => msg.record.partition) // grouped on queue partitions
-      .map { msg: CommittableMessage[String, String] =>
-        // TODO: handle error on sending a marker
-        ProducerMessage.single(
-          new ProducerRecord[MarkerKey, MarkerValue](markersTopic, MarkerKey.fromRecord(msg.record), new StartMarker(msg.record.timestamp() + messageTimeout)), //Note: handle redeliverAfter differently - save relative to message time
-          msg
-        )
+    Consumer.committablePartitionedSource(consumerSettings, Subscriptions.topics(queueTopic))
+      .mapAsyncUnordered(maxPartitions) {
+        case (topicPartition, source) =>
+          source
+            .map { msg: CommittableMessage[String, String] =>
+              ProducerMessage.single(
+                new ProducerRecord[MarkerKey, MarkerValue](markersTopic, MarkerKey.fromRecord(msg.record), new StartMarker(msg.record.timestamp() + messageTimeout)), //Note: handle redeliverAfter differently - save relative to message time
+                msg
+              )
+            }
+            .via(Producer.flexiFlow(markerProducerSettings)) // TODO: handle error on sending a marker; only commit on producer success; don't commit after failed message
+
+            .map(_.passThrough.committableOffset)
+            .runWith(Committer.sink(committerSettings))
       }
-      .via(Producer.flexiFlow(markerProducerSettings))
-      .map(_.passThrough.committableOffset)
-      .mergeSubstreams
-      .toMat(Committer.sink(committerSettings))(DrainingControl.apply)
+      .toMat(Sink.ignore)(DrainingControl.apply)
       .run()
   }
 }
