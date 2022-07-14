90bf036bec7caeba1f15f9cd979345a5f9676a74
Mariusz Krzemień <mkrzemien@gmail.com>
2022-07-14 09:28:45 +0200

    commit marker offsets stream - committablePartitionedSource

--- core/src/main/scala/com/softwaremill/kmq/redelivery/streams/CommitMarkerOffsetsStream.scala
+++ core/src/main/scala/com/softwaremill/kmq/redelivery/streams/CommitMarkerOffsetsStream.scala
@@ -6,6 +6,7 @@ import akka.kafka.ConsumerMessage.CommittableMessage
 import akka.kafka.scaladsl.Consumer.DrainingControl
 import akka.kafka.scaladsl.{Committer, Consumer}
 import akka.kafka.{CommitterSettings, ConsumerSettings, Subscriptions}
+import akka.stream.scaladsl.Sink
 import com.softwaremill.kmq.redelivery.Offset
 import com.softwaremill.kmq.{EndMarker, MarkerKey, MarkerValue, StartMarker}
 
@@ -16,38 +17,41 @@ class CommitMarkerOffsetsStream(markerConsumerSettings: ConsumerSettings[MarkerK
   def run(): DrainingControl[Done] = {
     val committerSettings = CommitterSettings(system)
 
-    Consumer.committableSource(markerConsumerSettings, Subscriptions.topics(markersTopic))
-      .groupBy(maxSubstreams = maxPartitions, f = msg => msg.record.partition) // Note: sorted not on msg.record.key.messagePartition
-      .statefulMapConcat { () => // keep track of open markers
-        val markersByOffset = new CustomPriorityQueueMap[MarkerKey, CommittableMessage[MarkerKey, MarkerValue]](valueOrdering = bySmallestOffsetAscending)
-        msg => {
-          msg.record.value match {
-            case _: StartMarker => markersByOffset.put(msg.record.key, msg)
-            case _: EndMarker => markersByOffset.remove(msg.record.key)
-            case _ => throw new IllegalArgumentException()
-          }
-          markersByOffset.headOption
-        }
+    Consumer.committablePartitionedSource(markerConsumerSettings, Subscriptions.topics(markersTopic))
+      .mapAsyncUnordered(maxPartitions) {
+        case (topicPartition, source) =>
+          source
+            .statefulMapConcat { () => // keep track of open markers
+              val markersByOffset = new CustomPriorityQueueMap[MarkerKey, CommittableMessage[MarkerKey, MarkerValue]](valueOrdering = bySmallestOffsetAscending)
+              msg => {
+                msg.record.value match {
+                  case _: StartMarker => markersByOffset.put(msg.record.key, msg)
+                  case _: EndMarker => markersByOffset.remove(msg.record.key)
+                  case _ => throw new IllegalArgumentException()
+                }
+                markersByOffset.headOption
+              }
+            }
+            .statefulMapConcat { () => // pass only markers with increasing offsets
+              val maxOffset = new CustomHolder[Offset]()
+              msg =>
+                if (maxOffset.get.fold(true)(_ < msg.record.offset)) {
+                  maxOffset.update(msg.record.offset)
+                  Some(msg)
+                }
+                else None
+            }
+            .statefulMapConcat { () => // for each new marker return previous one
+              val previousMsg = new CustomHolder[CommittableMessage[MarkerKey, MarkerValue]]()
+              msg =>
+                val prev = previousMsg.get
+                previousMsg.update(msg)
+                prev
+            }
+            .map(_.committableOffset)
+            .runWith(Committer.sink(committerSettings))
       }
-      .statefulMapConcat { () => // pass only markers with increasing offsets
-        val maxOffset = new CustomHolder[Offset]()
-        msg =>
-          if (maxOffset.get.fold(true)(_ < msg.record.offset)) {
-            maxOffset.update(msg.record.offset)
-            Some(msg)
-          }
-          else None
-      }
-      .statefulMapConcat { () => // for each new marker return previous one
-        val previousMsg = new CustomHolder[CommittableMessage[MarkerKey, MarkerValue]]()
-        msg =>
-          val prev = previousMsg.get
-          previousMsg.update(msg)
-          prev
-      }
-      .map(_.committableOffset)
-      .mergeSubstreams
-      .toMat(Committer.sink(committerSettings))(DrainingControl.apply)
+      .toMat(Sink.ignore)(DrainingControl.apply)
       .run()
   }
 
