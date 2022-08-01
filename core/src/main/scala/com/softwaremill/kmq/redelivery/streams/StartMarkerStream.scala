585541dbb456ba19070aaf0003f15a236db3f249
Mariusz Krzemień <mkrzemien@gmail.com>
2022-08-01 16:36:06 +0200

    fix StartMarkerStream - surround with RestartSource

--- core/src/main/scala/com/softwaremill/kmq/redelivery/streams/StartMarkerStream.scala
+++ core/src/main/scala/com/softwaremill/kmq/redelivery/streams/StartMarkerStream.scala
@@ -6,31 +6,43 @@ import akka.kafka.ConsumerMessage.CommittableMessage
 import akka.kafka.scaladsl.Consumer.DrainingControl
 import akka.kafka.scaladsl.{Committer, Consumer, Producer}
 import akka.kafka._
-import akka.stream.scaladsl.Sink
+import akka.stream.RestartSettings
+import akka.stream.scaladsl.{Flow, Keep, RestartSource, Sink}
 import com.softwaremill.kmq.{MarkerKey, MarkerValue, StartMarker}
 import org.apache.kafka.clients.producer.ProducerRecord
+
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 
 class StartMarkerStream(msgConsumerSettings: ConsumerSettings[String, String],
                         markerProducerSettings: ProducerSettings[MarkerKey, MarkerValue],
                         queueTopic: String, markersTopic: String, maxPartitions: Int, messageTimeout: Long)
                        (implicit system: ActorSystem) {
 
-  def run(): DrainingControl[Done] = {
+  def startMarkerSink(): Sink[CommittableMessage[String, String], Future[Done]] = {
     val committerSettings = CommitterSettings(system)
+
+    Flow[CommittableMessage[String, String]]
+      .map { msg: CommittableMessage[String, String] =>
+        ProducerMessage.single(
+          new ProducerRecord[MarkerKey, MarkerValue](markersTopic, MarkerKey.fromRecord(msg.record), new StartMarker(msg.record.timestamp() + messageTimeout)),
+          msg
+        )
+      }
+      .via(Producer.flexiFlow(markerProducerSettings))
+      .map(_.passThrough.committableOffset)
+      .toMat(Committer.sink(committerSettings))(Keep.right)
+  }
+
+  def run(): DrainingControl[Done] = {
+    val restartSettings = RestartSettings(minBackoff = 3.seconds, maxBackoff = 30.seconds, randomFactor = 0.2)
 
     Consumer.committablePartitionedSource(msgConsumerSettings, Subscriptions.topics(queueTopic))
       .mapAsyncUnordered(maxPartitions) {
         case (topicPartition, source) =>
-          source
-            .map { msg: CommittableMessage[String, String] =>
-              ProducerMessage.single(
-                new ProducerRecord[MarkerKey, MarkerValue](markersTopic, MarkerKey.fromRecord(msg.record), new StartMarker(msg.record.timestamp() + messageTimeout)), //Note: handle redeliverAfter differently - save relative to message time
-                msg
-              )
-            }
-            .via(Producer.flexiFlow(markerProducerSettings)) // TODO: handle error on sending a marker - don't commit after producer failure
-            .map(_.passThrough.committableOffset)
-            .runWith(Committer.sink(committerSettings))
+          RestartSource.withBackoff(restartSettings)(() => source)
+            .toMat(startMarkerSink())(Keep.right)
+            .run()
       }
       .toMat(Sink.ignore)(DrainingControl.apply)
       .run()
