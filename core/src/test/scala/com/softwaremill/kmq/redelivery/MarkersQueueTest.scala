828dd0c79b28250534a7473c578a37ac8f447fbf
Mariusz Krzemień <mkrzemien@gmail.com>
2022-08-02 10:15:27 +0200

    refactor MarkersQueue using PriorityQueueMap

--- core/src/test/scala/com/softwaremill/kmq/redelivery/MarkersQueueTest.scala
+++ core/src/test/scala/com/softwaremill/kmq/redelivery/MarkersQueueTest.scala
@@ -32,4 +32,32 @@ class MarkersQueueTest extends AnyFlatSpec {
     toRedeliver should have size 2
     toRedeliver.map(_.getMessageOffset).toSet should be(Set(1001L, 1000L))
   }
+
+  it should "provide smallest marker offset" in {
+    // given
+    val mq = new MarkersQueue(0)
+    mq.handleMarker(100, new MarkerKey(1, 1000), new StartMarker(4000L), 10000L)
+    mq.handleMarker(101, new MarkerKey(1, 1001), new StartMarker(4000L), 12000L)
+
+    // when
+    val offset = mq.smallestMarkerOffset()
+
+    // then
+    offset shouldBe Some(100)
+  }
+
+  it should "provide smallest marker offset despite dequeue" in {
+    // given
+    val mq = new MarkersQueue(0)
+    mq.handleMarker(100, new MarkerKey(1, 1000), new StartMarker(4000L), 10000L)
+    mq.handleMarker(101, new MarkerKey(1, 1001), new StartMarker(4000L), 12000L)
+
+    // when
+    val toRedeliver = mq.markersToRedeliver(15000)
+    val offset = mq.smallestMarkerOffset()
+
+    // then
+    toRedeliver.head.getMessageOffset should be (1000)
+    offset shouldBe Some(100)
+  }
 }
